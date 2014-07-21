8a8960a81ea8b9b3fc41fabd4834f9425b0ff235
Ilya Kantor <iliakan@gmail.com>
2014-07-21 10:36:06 +0400

    rewrite, make more flexible

--- Readme.md
+++ Readme.md
@@ -1,14 +1,23 @@
 
-# Deprecated
+# Fast cached jshint for gulp
 
-Gulp task for linting which makes process exit with non-zero status on fail.
-
-Can be used with git pre-commit hook to deny bad javascript.
+It uses in memory stat cache, that's why `lint-watch` is blazing fast.
 
 Usage:
-```
-var gulpTaskLint = require('javascript-gulp-task-lint');
-gulp.task('lint', gulpTaskLint(['**/*.js', '!node_modules/**', '!app/**', '!tutorial/**']));
+```js
+var sources = './**/*.js';
+
+gulp.task('lint', gp.jshintCache({ src: sources }));
+
+gulp.task('lint-or-die', gp.jshintCache({ src: sources, dieOnError: true }));
+
+gulp.task('lint-watch', ['lint'], function(neverCalled) {
+  gulp.watch(serverSources, ['lint']);
+});
 ```
 
-In `jshintrc` you can find `.jshintrc` for the project and for mocha + should.js tests.
+Options:
+  - `sources`* -- sources for `glob.src`
+  - `dieOnError` -- if true, the process will die with error status `1` if lint fails. Use this for git pre-commit check.
+  - `jshint` -- options for `gulp-jshint`
+
--- index.js
+++ index.js
@@ -1,42 +1,72 @@
 var gulp = require('gulp');
-var gutil = require('gulp-util');
-var map = require('map-stream');
-var jshint = require('gulp-jshint');
-var fs = require('fs');
+var path = require('path');
+var gp = require('gulp-load-plugins')({
+  config: path.join(__dirname, 'package.json')
+});
+var getContents = require('vinyl-fs/lib/src/getContents');
+var through2 = require('through2');
+var lazypipe = require('lazypipe');
 
-var exitCode = 0;
 
-module.exports = function(src) {
-  var totalLintErrors = null;
+/**
+ * Lint task
+ * - caches jshint result on files until they are modified
+ */
+module.exports = function(options) {
 
-  return function(callback) {
-    gulp.src(src)
-      .pipe(jshint())
-      .pipe(jshint.reporter('default'))
-      .pipe(map(function(file, cb) {
-        if (!file.jshint.success) {
-          totalLintErrors += file.jshint.results.length;
-          exitCode = 1;
-        }
-        cb(null, file);
-      }))
-      .on('end', function() {
+  var cache = {};
 
-        var errString = totalLintErrors + '';
-        if (exitCode) {
-          console.log(gutil.colors.magenta(errString), 'errors\n');
-          process.emit('lint-fail');
+// read file and jshint - only when needed
+  var jshintChannel = lazypipe()
+    .pipe(getContents.bind(null, {})) // notice the stream function has not been called!
+    .pipe(gp.jshint.bind(null, options.jshint));
+
+  return function() {
+
+    var hadErrors = false;
+
+    // when running in watch, it's important not to read all files
+    var stream = gulp.src(options.src, {read: false})
+      .pipe(through2.obj(function(source, enc, callback) {
+        // pass down a file from cache OR source if MISS
+        if (cache[source.path] && source.stat.mtime <= cache[source.path].stat.mtime) {
+          this.push(cache[source.path]);
+        } else {
+          this.push(source);
         }
         callback();
-      });
-  }
-}
+      }))
+      .pipe(gp.if(function(file) {
+        // IF file NOT from cache THEN read && jshint it
+        return !file.fromLintCache;
+      }, jshintChannel()))
+      .pipe(through2.obj(function(source, enc, callback) {
+        // cache the file
+        cache[source.path] = source;
+        cache[source.path].fromLintCache = true;
 
+        if (!source.jshint.success) {
+          hadErrors = true;
+        }
 
-process.on('lint-fail', function() {
-  process.nextTick(function() {
-    var msg = "gulp '" + gulp.seq + "' failed";
-    console.log(gutil.colors.red(msg));
-    process.exit(exitCode);
-  });
-});
+        this.push(source);
+        callback();
+      }))
+      .pipe(gp.jshint.reporter('default'));
+
+    if (options.dieOnError) {
+      stream
+        // consume all
+        .on('readable', function() {
+          this.read()
+        })
+        // check the result
+        .on('end', function() {
+          if (hadErrors) process.exit(1);
+        });
+    }
+
+    return stream;
+  };
+
+};
